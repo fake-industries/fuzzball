b4f029cc50be4c06e15e9b11dddeab227c27517b
Kesara Rathnayake <kesara@fq.nz>
2020-08-28 01:55:26 +1200

    Add volume control and tweak fuzz effect.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 #[macro_use]
 extern crate vst;
-extern crate rand;
 
-use rand::random;
 use vst::buffer::AudioBuffer;
 use vst::plugin::{Category, Info, Plugin, PluginParameters};
 use vst::util::AtomicFloat;
 
 use std::sync::Arc;
+
+const MINIMUM_FUZZ: f32 = 0.01;
+const MAXIMUM_FUZZ: f32 = 1.95;
 
 struct FuzzBall {
     params: Arc<FuzzBallParameters>,
@@ -15,6 +16,7 @@ struct FuzzBall {
 
 struct FuzzBallParameters {
     fuzz: AtomicFloat,
+    volume: AtomicFloat,
 }
 
 impl Default for FuzzBall {
@@ -25,7 +27,10 @@ impl Default for FuzzBall {
 
 impl Default for FuzzBallParameters {
     fn default() -> FuzzBallParameters {
-        FuzzBallParameters { fuzz: AtomicFloat::new(0.5) }
+        FuzzBallParameters {
+            fuzz: AtomicFloat::new(0.5),
+            volume: AtomicFloat::new(0.5),
+        }
     }
 }
 
@@ -38,17 +43,24 @@ impl Plugin for FuzzBall {
             version: 1,
             inputs: 2,
             outputs: 2,
-            parameters: 1,
+            parameters: 2,
             category: Category::Effect,
             ..Default::default()
         }
     }
 
     fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
-        let fuzz = self.params.fuzz.get();
+        let mut fuzz = self.params.fuzz.get();
+        if fuzz > MAXIMUM_FUZZ {
+            fuzz = MAXIMUM_FUZZ;
+        } else if fuzz < MINIMUM_FUZZ {
+            fuzz = MINIMUM_FUZZ;
+        }
+        let volume = self.params.volume.get();
         for (input_buffer, output_buffer) in buffer.zip() {
             for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
-                *output_sample = *input_sample * (fuzz - (random::<f32>() * fuzz));
+                *output_sample = *input_sample * (volume) *
+                    (fuzz.log(*input_sample) + input_sample.log(fuzz));
             }
         }
     }
@@ -62,21 +74,23 @@ impl PluginParameters for FuzzBallParameters {
     fn get_parameter(&self, index: i32) -> f32 {
         match index {
             0 => self.fuzz.get(),
+            1 => self.volume.get(),
             _ => 0.0,
         }
     }
 
     fn set_parameter(&self, index: i32, val: f32) {
-        #[allow(clippy::single_match)]
         match index {
             0 => self.fuzz.set(val),
+            1 => self.volume.set(val),
             _ => (),
         }
     }
 
     fn get_parameter_text(&self, index: i32) -> String {
         match index {
-            0 => format!("{:.2}", (self.fuzz.get() - 0.5) * 2f32),
+            0 => format!("{:.2}", self.fuzz.get() * 2f32),
+            1 => format!("{:.2}", (self.volume.get() - 0.5) * 2f32),
             _ => "".to_string(),
         }
     }
@@ -84,6 +98,7 @@ impl PluginParameters for FuzzBallParameters {
     fn get_parameter_name(&self, index: i32) -> String {
         match index {
             0 => "fuzz",
+            1 => "volume",
             _ => "",
         }.to_string()
     }
